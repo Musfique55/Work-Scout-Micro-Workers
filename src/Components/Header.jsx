fb15036896e85a607469a262f07647499b38aa31
Musfique55 <mdmusfiquepatwary@gmail.com>
2024-06-01 17:59:26 +0600

    added dynamic design to header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,10 +2,19 @@ import { Link, NavLink } from 'react-router-dom';
 import logo from '../assets/logo.png';
 import { FaUnlock } from 'react-icons/fa';
 import { FaCirclePlus } from 'react-icons/fa6';
+import { useEffect, useState } from 'react';
 const Header = () => {
+    const [scroll,setScroll] = useState(0);
+    useEffect(() => {
+        const handleScroll = () => {
+            setScroll(window.scrollY);
+        }
+        window.addEventListener('scroll',handleScroll)        
+    },[scroll]);
+    console.log(scroll);
     return (
-        <div >
-            <div className='flex justify-between items-center px-12 py-8 fixed w-full z-50'>
+        <div  className=''>
+            <div id='header' className={`flex justify-between items-center px-12 py-8 fixed w-full z-50 ${scroll > 150 ? 'bg-white text-black shadow-lg' : 'text-black bg-transparent'}`}>
                 <Link to='/'>
                 <img src={logo} alt="" className='w-[193px] h-[40px] cursor-pointer'/>
                 </Link>
@@ -15,11 +24,11 @@ const Header = () => {
                     </NavLink>
                 </div>
                 <div>
-                    <NavLink to='/login' className="hover:text-[rgb(38,174,97)] text-white font-base font-medium">
+                    <NavLink to='/login' className="hover:text-[rgb(38,174,97)]  font-base font-medium">
                         <FaUnlock></FaUnlock>
                         Login
                     </NavLink>
-                    <NavLink to='/register' className="hover:text-[rgb(38,174,97)] text-white font-base font-medium">
+                    <NavLink to='/register' className="hover:text-[rgb(38,174,97)]  font-base font-medium">
                         <FaCirclePlus />
                         Register
                     </NavLink>
